ff741566eec66970dcc16795b65bda696633f511
Mariel <mariel.dominguez.1988@gmail.com>
2023-03-20 10:29:55 -0300

    Primer commit

---
+++ algoritmo.js
@@ -0,0 +1,7 @@
+document.querySelector("body").innerHTML=`
+<h1> Practica 1-1</h1>
+<h2></h2>
+<h3></h3>
+<img src="https://s3.ap-southeast-1.amazonaws.com/we-xpats.com/uploads/article/5571/pt_33_2.jpg" alt="perrito">
+<img src="https://www.muyinteresante.com.mx/wp-content/uploads/2018/05/httpstved-prod.adobecqms.netcontentdamtbgmexicomuyinteresantemxcuriosidadestop171225perro.imgo_.jpg"
+    alt="perrito2"></img>`;
